3325f87e5d812d4fe2617fe145690159dc173d2a
Rajkumar <rajkumaraws@gmail.com>
2017-06-18 13:17:40 +0530

    changes happened in java file

--- JavaProjectTest/src/org/app/Java/JavaTest.java
+++ JavaProjectTest/src/org/app/Java/JavaTest.java
@@ -4,6 +4,10 @@ public class JavaTest {
 
 	public static void main(String[] args) {
 		System.out.println("Testttting java");
+		
+		System.out.println(" Testting is done ");
+		
+		System.out.println("Welcome to GIT Repo");
 
 	}
 
